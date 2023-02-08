7292a479b834c40fe4b6e3f3b272adfdc035a785
seeker11 <jmesabong@gmail.com>
2023-02-08 14:04:09 -0500

    Agregando formulario de instertar tareas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
+import FormularioTareas from './components/FormularioTareas';
 
 const App = () => {
+  const[tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'lavar la ropa',
+        completada: false
+      },
+      {
+        id: 1,
+        texto: 'salir de paseo',
+        completada: false
+      }
+    ]
+  );
+  console.log(tareas);
   return (
     <div className="contenedor">
-      <Header />      
+      <Header />   
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />   
     </div>
   );
 }
---
+++ src/components/FormularioTareas.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import { v4 as uuidv4 } from 'uuid';
+
+const FormularioTareas = ({tareas, cambiarTareas}) => {
+    const [inputTarea, cambiarInputTarea] = useState('');
+
+    const handleInput = (e) => {
+        cambiarInputTarea(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        cambiarTareas(
+            [
+                ...tareas,
+                {
+                    id: uuidv4(),
+                    texto: inputTarea,
+                    completada: false
+                }
+            ]
+        );
+    }
+    return ( 
+        <form action="" className="formulario-tareas" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                className="formulario-tareas__input" 
+                placeholder="Escribe un tarea"
+                value={inputTarea}
+                onChange={(e) => handleInput(e)} 
+            />
+            <button
+                type="submit"
+                className="formulario-tareas__btn"
+            >
+                <FontAwesomeIcon icon={faPlusSquare} className="formulario-tareas__icono-btn" />
+            </button>
+        </form>
+     );
+}
+ 
+export default FormularioTareas;
