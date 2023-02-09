70eb0c3dae37a778f79b898c20105c8ece49fb57
seeker11 <jmesabong@gmail.com>
2023-02-09 16:33:06 -0500

    Agregando funcionalidad de gusradar datos en localStorage y app terminada

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import FormularioTareas from './components/FormularioTareas';
 import ListaTareas from './components/ListaTareas';
 
 const App = () => {
-  const[tareas, cambiarTareas] = useState(
-    [
-      {
-        id: 1,
-        texto: 'lavar la ropa',
-        completada: false
-      },
-      {
-        id: 2,
-        texto: 'salir de paseo',
-        completada: true
-      },
-      {
-        id: 3,
-        texto: 'tarea para eliminar',
-        completada: true
-      }
-    ]
-  );
-  const [mostarCompletadas, cambiarMostarCompletadas] = useState(true);
+  const tareasGuardadas = localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : [];
+
+  const[tareas, cambiarTareas] = useState(tareasGuardadas);
+  
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas))
+  }, [tareas]);
+
+  let mostarCompletadasGuardadas;
+  if (localStorage.getItem('mostarCompletadas') === null) {
+      mostarCompletadasGuardadas = true;
+  } else {
+    mostarCompletadasGuardadas = localStorage.getItem('mostarCompletadas') === 'true';
+  }
+
+  const [mostarCompletadas, cambiarMostarCompletadas] = useState(mostarCompletadasGuardadas);
+  
+  useEffect(() => {
+    localStorage.setItem('mostarCompletadas', mostarCompletadas.toString())
+  }, [mostarCompletadas]);
+
   return (
     <div className="contenedor">
       <Header mostarCompletadas={mostarCompletadas} cambiarMostarCompletadas={cambiarMostarCompletadas} />   
